100fbdc1abbde9f376bc85f1e471e8e04a7c2518
water-zi <928069358@qq.com>
2025-04-05 21:05:25 +0800

    Initial Commit

---
+++ WAUPS AUTO/WAUPS_AUTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAUPS_AUTOApp.swift
+//  WAUPS AUTO
+//
+//  Created by 唐梓皓 on 2025/4/5.
+//
+
+import SwiftUI
+
+@main
+struct WAUPS_AUTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
